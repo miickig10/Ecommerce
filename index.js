179270a87489740de0ac4c9a6dc76c9c0d8d2f51
Micaela Girado <giramica1@gmail.com>
2022-05-03 17:53:55 -0300

    Agrego todas las hamburguesas
    Agrego las hamburguesas y las inyecto en el html
    
    Co-Authored-By: Andreina Nathaly Rodriguez Martinez <66847303+NathalyStudio@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
-const API_URL = "https://ig-food-menus.herokuapp.com/<params>"
+const API_URL = async() => { //await me sirve para esperar a que mi url termine de cargar
+    //await funcina solamente con funciones asincronicas por eso uso async.
+    try {
+        const respuesta = await fetch("https://ig-food-menus.herokuapp.com/burgers"); // Fetch nos permite acceder y manipular contenido http
+        console.log(respuesta); // La respuesta a mi petición van a ser todas las hamburguesas
 
-console.log(API_URL)
+        if (respuesta.status === 200) { // El codigo 200 me dice que esta todo Ok y procedo a acceder al contenido
+            const datos = await respuesta.json(); // Los datos que obtengo son .json
 
-var burguers = document.querySelector('#burguers'); // Esto lo traigo del html donde cree el div
+            let burgers = ""; // Todos los datos que obtenga los voy a guardar en mi variable BURGERS
+            datos.forEach(burger => { // Por cada burguer voY acceder al titulo, la foto, el pais y el precio.
+                burgers += `
+                <div class="burger-container">
+                     <h2>${burger.name}</h2>
+                     <div class = "burger">
+                       <img class = "foto" src="${burger.img}">
+                     </div>
+                     <p> Procedencia: ${burger.country}</p>
+                     <h3>Precio $ ${burger.price}</h3>
 
-fetch('https://ig-food-menus.herokuapp.com/burgers')
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data['0'])
-        burguers.innerHTML = `
-            <img src="${data['0'].img}">
-            <p>Plato: ${data['0'].name} </p>
-            <p>Procedencia: ${data['0'].country}</p>
-            <p>Precio: $ ${data['0'].price}</p>
-        
-        `
+                </div>
+                     `
+            });
 
-    });
+            document.getElementById('burgers1').innerHTML = burgers; // Todo lo que obtuve lo inyecto en mi html de forma dinÁmica.
 
+        } else if (respuesta === 404) { // Si la pagina no entró por el cofigo 200, puede largar error 404
+            console.log("La página de destino no existe")
+        } else {
+            console.log("Error!!") // si tenemos un error desconocido puede salir esto.
+        }
 
-fetch('https://ig-food-menus.herokuapp.com/desserts')
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+    } catch (error) { // can catch capturamos el error desde consola e intentamos solucionarlo.
+        console.log(error)
+    }
+}
 
 
-fetch('https://ig-food-menus.herokuapp.com/drinks')
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+API_URL(); // Es la respuesta a la llamada de API
 
-/*  ya estoy aqui*/
+//PD: ESPERO SE ENTIENDA JAJAJA
+/* sii, esta genial */
